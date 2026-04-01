b00d37c69666f923de7ac55a5d6c7471b46de783
Tatiana <t.ya14091984@gmail.com>
2026-04-01 13:13:08 +0300

    Refactor: convert fetchWeather to async/await for cleaner error handling

--- src/feachers/api/weatherAction.js
+++ src/feachers/api/weatherAction.js
@@ -2,24 +2,21 @@ import {apiKey, baseURL} from "../../utils/const.js";
 import {putWeather} from "../weather/weatherSlice.js";
 import {setMessage} from "../message/messageSlice.js";
 
-export const fetchWeather = (city) => {
-    return (dispatch) => {
-        fetch(`${baseURL}?q=${city}&appid=${apiKey}&units=metric`)
-            .then(response => response.json())
-            .then(data => {
-                dispatch(putWeather({
-                    country: data.sys.country,
-                    city: data.name,
-                    temp: data.main.temp,
-                    pressure: data.main.pressure,
-                    sunset: data.sys.sunset * 1000,
-                }))
-                dispatch(setMessage(''))
-            })
-            .catch(error => {
-                console.log(error)
-                dispatch(setMessage('Enter correct city name'))
-                dispatch(putWeather({}))
-            });
-    };
+export const fetchWeather = (city) => async dispatch => {
+    try{
+    const res = await fetch(`${baseURL}?q=${city}&appid=${apiKey}&units=metric`)
+    const data = await res.json();
+    dispatch(putWeather({
+        country: data.sys.country,
+        city: data.name,
+        temp: data.main.temp,
+        pressure: data.main.pressure,
+        sunset: data.sys.sunset * 1000,
+    }))
+    dispatch(setMessage(''))
+    } catch (e) {
+        console.log(e)
+        dispatch(setMessage('Enter correct city name'))
+        dispatch(putWeather({}))
+    }
 };
